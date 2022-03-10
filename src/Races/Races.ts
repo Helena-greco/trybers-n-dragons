0d9cc5750648abbddc424815ca3d6e24188dd39f
Helena <nenafgreco@hotmail.com>
2022-03-10 20:10:46 -0300

    Criando metodo createdRacesInstances e getter maxLifePoints

--- src/Races/Races.ts
+++ src/Races/Races.ts
@@ -1,5 +1,11 @@
 abstract class Race {
   constructor(readonly name: string, readonly dexterity: number) { }
+
+  static createdRacesInstances(): void {
+    throw new Error('Not implemented');
+  }
+  
+  abstract get maxLifePoints(): number;
 }
 
 export default Race;
