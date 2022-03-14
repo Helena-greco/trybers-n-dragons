81e19fdbc775400131e2a81e7ff3b4ea7feb7558
Helena <nenafgreco@hotmail.com>
2022-03-14 11:41:09 -0300

    Requisito 6: interface Fighter

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,12 @@
+import Energy from '../Energy';
+
+export default interface Fighter {
+  lifePoints: number,
+  strength: number,
+  defense: number,
+  energy?: Energy, // opcional
+  attack(enemy: Fighter): void,
+  special(enemy: Fighter): void,
+  levelUp(): void,
+  receiveDamage(attackPoints: number): void,
+}
